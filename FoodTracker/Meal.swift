54d528b66c00be9a45800ad28b105d6b1ff25673
Juan José Ramírez Calderón <juan.ramirez@tektonlabs.com>
2016-10-31 14:43:50 -0500

    FoodTracker: Add meal model with tests

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,30 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Juanjo on 10/31/16.
+//  Copyright © 2016 Tektonlabs. All rights reserved.
+//
+
+import UIKit //this also includes Foundation
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+}
