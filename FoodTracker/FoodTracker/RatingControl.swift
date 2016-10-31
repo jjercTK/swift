04717b5c1bc4ad2c9d2cc0372c13edc6347daf60
Juan José Ramírez Calderón <juan.ramirez@tektonlabs.com>
2016-10-31 14:18:47 -0500

    Add rating controller to storyboard preview

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -8,35 +8,73 @@
 
 import UIKit
 
+@IBDesignable
 class RatingControl: UIView {
     // MARK: Properties
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            //setNeedsLayout()
+            updateButtonSelectionStates()
+        }
+    }
     var ratingButtons = [UIButton]()
     let spacing = 5
     let starCount = 5
-    let buttonSize = 44
+    //let buttonSize = 44 <- this was added to the view as a constraint
     
-    // MARK: Initialization
-    required init?(coder aDecoder: NSCoder) {
-        
-        super.init(coder: aDecoder)
-        
+    @IBInspectable var emptyImage: UIImage! = UIImage(named: "emptyStar") {
+        didSet {
+            for button in ratingButtons {
+                button.setImage(emptyImage, for: .normal)
+            }
+        }
+    }
+    
+    @IBInspectable var filledImage: UIImage! = UIImage(named: "filledStar") {
+        didSet {
+            for button in ratingButtons {
+                button.setImage(filledImage, for: .selected)
+                button.setImage(filledImage, for: [.selected, .highlighted])
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    func setup(){
         for _ in 0..<starCount {
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+            //button.backgroundColor = UIColor.red
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped), for: .touchDown)
+            //button.setImage(emptyImage, for: .normal)
+            //button.setImage(filledImage, for: .selected)
+            //button.setImage(filledImage, for: [.selected, .highlighted])
+            button.adjustsImageWhenHighlighted = false
             ratingButtons += [button]
             addSubview(button)
         }
     }
     
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
     override var intrinsicContentSize : CGSize {
+        let buttonSize = Int(frame.size.height)
         let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
         return CGSize(width: width, height: buttonSize)
     }
     
     override func layoutSubviews() {
+        let buttonSize = Int(frame.size.height)
         var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
         for (index, button) in ratingButtons.enumerated() {
             buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
             button.frame = buttonFrame
@@ -45,7 +83,13 @@ class RatingControl: UIView {
     
     // MARK: Button Action
     func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        rating = ratingButtons.index(of: button)! + 1
+    }
+    
+    func updateButtonSelectionStates() {
+        for (index,button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+        }
     }
 
 }
