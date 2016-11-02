552d0a7cb1c49618cac9d7f2db00862541aadf05
Juan José Ramírez Calderón <juan.ramirez@tektonlabs.com>
2016-11-02 12:12:55 -0500

    FoodTracker: Cancel a New Meal Addition

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -53,6 +53,9 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     }
     
     // MARK: Navigation
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
     
     override func  prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let button = sender as? UIBarButtonItem, button == saveButton {
